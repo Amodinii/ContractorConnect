6e76f75ae91a9f4eaec08612aa493c8a57503fe3
ParthChandel165 <parth.chandel20@gmail.com>
2024-02-26 11:44:27 +0530

    Updated homepage

--- HomePage/Homepage.js
+++ HomePage/Homepage.js
@@ -2,6 +2,7 @@ let scrollContainer = document.querySelector(".gallery");
 let backButton = document.getElementById("back");
 let frontButton = document.getElementById("front");
 let imageWidth = scrollContainer.querySelector("img").clientWidth;
+let AccountButton = document.getElementById("Account");
 
 scrollContainer.addEventListener("wheel",(evt)=>{
     evt.preventDefault();
@@ -15,4 +16,8 @@ frontButton.addEventListener("click",()=>{
 
 backButton.addEventListener("click",()=>{
     scrollContainer.scrollLeft-= imageWidth;
+});
+AccountButton.addEventListener("click", () => {
+    var dropdown = document.getElementsByClassName("AccountDrop")[0];
+    dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
 });
