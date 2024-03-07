6ec952afde4bba841a7de09338dad028bb98b83b
Amodini A P <142674727+Amodinii@users.noreply.github.com>
2024-03-07 19:36:02 +0530

    Update Homepage.js

--- HomePage/Homepage.js
+++ HomePage/Homepage.js
@@ -1,23 +1,18 @@
-let scrollContainer = document.querySelector(".gallery");
-let backButton = document.getElementById("back");
-let frontButton = document.getElementById("front");
-let imageWidth = scrollContainer.querySelector("img").clientWidth;
 let AccountButton = document.getElementById("Account");
-
-scrollContainer.addEventListener("wheel",(evt)=>{
-    evt.preventDefault();
-    let scrollAmount = evt.deltaY > 0 ? imageWidth : -imageWidth;
-    scrollContainer.scrollLeft += scrollAmount;
-});
-
-frontButton.addEventListener("click",()=>{
-    scrollContainer.scrollLeft+= imageWidth;
-});
-
-backButton.addEventListener("click",()=>{
-    scrollContainer.scrollLeft-= imageWidth;
-});
 AccountButton.addEventListener("click", () => {
     var dropdown = document.getElementsByClassName("AccountDrop")[0];
     dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
 });
+
+// AUTOMATIC NAVIGATION OF IMAGE SLIDER
+let counter = 1;
+
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    console.log('Counter: ' + counter);
+    counter++;
+    if(counter > 5) {
+        counter = 1; //Reset counter
+    }
+}, 5000);
+
