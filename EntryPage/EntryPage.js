19bc21c3a7d0667d9fe020f7cdc3394016df0de1
nottpande <adityapande069@gmail.com>
2024-03-15 23:29:52 +0530

    Chnages at homepage

--- EntryPage/EntryPage.js
+++ EntryPage/EntryPage.js
@@ -29,4 +29,53 @@ function closePopup() {
   
 // Setting timeout to show the pop-up after 1 minute
 setTimeout(showPopup, 60000); // 1 minute in milliseconds
-  
+
+// Add this JavaScript function to toggle the dropdown
+function toggleJoinDropdown() {
+    var dropdown = document.getElementById("joinDropdown");
+    dropdown.classList.toggle("show");
+}
+
+// Add this JavaScript to close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('#joinNowBtn')) {
+        var dropdowns = document.getElementsByClassName("joinDropdown");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+window.onscroll = function() {
+    var joinNowBtn = document.getElementById("joinNowBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        joinNowBtn.style.display = "none";
+    } else {
+        joinNowBtn.style.display = "block";
+    }
+}
+
+// Add this JavaScript to show/hide the dropdown on hover
+var joinNowBtn = document.getElementById("joinNowBtn");
+var joinDropdown = document.getElementById("joinDropdown");
+
+joinNowBtn.addEventListener("mouseover", function() {
+    joinDropdown.classList.add("show");
+});
+
+joinNowBtn.addEventListener("mouseout", function() {
+    joinDropdown.classList.remove("show");
+});
+
+joinDropdown.addEventListener("mouseover", function() {
+    joinDropdown.classList.add("show");
+});
+
+joinDropdown.addEventListener("mouseout", function() {
+    joinDropdown.classList.remove("show");
+});
+
+
