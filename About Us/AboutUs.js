9e5c5050bab2a1a8047902362fbad94744b5c0ec
Parth Chandel <parth.chandel20@gmail.com>
2024-03-27 23:29:37 +0530

    updated Changes

--- About Us/AboutUs.js
+++ About Us/AboutUs.js
@@ -3,3 +3,18 @@ AccountButton.addEventListener("click", () => {
     var dropdown = document.getElementsByClassName("AccountDrop")[0];
     dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
 });
+
+function toggleOptions(container) {
+    var options = container.querySelector('.options');
+    options.classList.toggle('show');
+}
+
+// Get all image containers
+var imageContainers = document.querySelectorAll('.image-container');
+
+// Add click event listener to each image container
+imageContainers.forEach(function(container) {
+    container.addEventListener('click', function() {
+        toggleOptions(container);
+    });
+})
