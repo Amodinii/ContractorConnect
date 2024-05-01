8df9c92fbb3fa0d1dd925a7dc156e577b8db98c2
Mihirchandra <mihirloke05@gmail.com>
2024-05-02 02:40:59 +0530

    your quotations

---
+++ public/quotations/quotation.js
@@ -0,0 +1,16 @@
+fetch("/quotation/data")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((entry) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${entry._id}</td>
+        <td>${entry.tender}</td>
+        <td>${entry.contractor}</td>
+        <td>${entry.title}</td>
+        <td><a href="http://localhost:5000/uploads/Quotations/${entry.title}">View Quotations</a></td>
+      `;
+      document.getElementById("quotation-table-body").appendChild(row);
+    });
+  })
+  .catch((error) => console.error("Error fetching data:", error));
