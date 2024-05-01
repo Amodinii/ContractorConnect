342be8a8757e502ffb8d63b2977cc86891424a76
Amodinii <apamodini@gmail.com>
2024-05-02 00:22:42 +0530

    thoda aur

--- public/HomePage_Vendor/js/index.js
+++ public/HomePage_Vendor/js/index.js
@@ -1,3 +1,43 @@
+function setCookie(cookieName, cookieValue, expirationDays) {
+    var d = new Date();
+    d.setTime(d.getTime() + expirationDays * 24 * 60 * 60 * 1000);
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+// Function to get the value of a cookie by name
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(";");
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i];
+        while (cookie.charAt(0) == " ") {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) == 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+// Function to check if the welcome message should be displayed
+function checkWelcomeCookie() {
+    var welcomeMessageCookie = getCookie("welcome_message");
+    if (welcomeMessageCookie != "") {
+        alert("Welcome back to our page!");
+    } else {
+        setCookie("welcome_message", "shown", 1); // Cookie expires in 1 day
+    }
+}
+
+// Call the checkWelcomeCookie function when the page loads
+window.onload = checkWelcomeCookie;
+
+// Retrieve JWT token from localStorage
+const token = localStorage.getItem('jwtToken');
+
 // Check if token exists
 if (token) {
     // Fetch user details with JWT token
@@ -29,3 +69,12 @@ if (token) {
 } else {
     console.error('JWT token not found in localStorage');
 }
+
+// Logout event listener
+document.getElementById("logout1").addEventListener("click", function () {
+    console.log("Logging out");
+    // Clear JWT token from localStorage
+    localStorage.removeItem('jwtToken');
+    // Redirect to sign-in page
+    window.location.href = "../Account_Creation/Sign_In/SignPage.html";
+});
